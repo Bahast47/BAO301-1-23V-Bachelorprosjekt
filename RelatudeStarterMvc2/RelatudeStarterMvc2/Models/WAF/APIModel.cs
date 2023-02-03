bb048cfcebe02bc31242153a408ab289028f1fe3
Vithusan1999 <70209043+Vithusan1999@users.noreply.github.com>
2023-02-03 13:24:52 +0100

    Relatude starter

---
+++ RelatudeStarterMvc2/RelatudeStarterMvc2/Models/WAF/APIModel.cs
@@ -0,0 +1,10 @@
+// *******************************************
+//          W A F - AUTO GENERATED CODE       
+// Do not edit! File is regularly overwritten.
+// *******************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WAF.API { public class CustomDefinition { } };
