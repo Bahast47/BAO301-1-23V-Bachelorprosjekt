44ff5967f01ee485b7d229e41b52902a50fb439d
Vithusan1999 <70209043+Vithusan1999@users.noreply.github.com>
2023-02-06 12:25:36 +0100

    DataModel

---
+++ BachelorProsject2023/BachelorProsject2023/Models/WAF/APIModel.cs
@@ -0,0 +1,255 @@
+// *******************************************
+//          W A F - AUTO GENERATED CODE       
+// Do not edit! File is regularly overwritten.
+// *******************************************
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace WAF.API { public class CustomDefinition{ } };
+
+namespace WAF.API.MySite {
+[ClassGuid("fdb92bac-b379-43ec-b327-02cc014b0ac5")]
+public class Course : WAF.API.Native.HierarchicalContent {
+public Course(IContentDataProvider dataProvider)
+: base(dataProvider) {}
+        static Guid _startGuid = new Guid("f18db84f-6f95-4769-ac35-3e441378faa0");
+        [PropertyGuid("f18db84f-6f95-4769-ac35-3e441378faa0")]
+        public DateTime Start {
+        get { return (DateTime)DataProvider.GetPropertyValue(_startGuid); }
+        set { DataProvider.SetPropertyValue(_startGuid, value); }
+        }
+      
+        static Guid _endGuid = new Guid("2d42b3bd-d02e-4f66-aa34-79f983405cbc");
+        [PropertyGuid("2d42b3bd-d02e-4f66-aa34-79f983405cbc")]
+        public DateTime End {
+        get { return (DateTime)DataProvider.GetPropertyValue(_endGuid); }
+        set { DataProvider.SetPropertyValue(_endGuid, value); }
+        }
+      
+        static Guid _registeringDateGuid = new Guid("e3a7c1e1-e453-4966-bd7b-c73d4537c999");
+        [PropertyGuid("e3a7c1e1-e453-4966-bd7b-c73d4537c999")]
+        public DateTime RegisteringDate {
+        get { return (DateTime)DataProvider.GetPropertyValue(_registeringDateGuid); }
+        set { DataProvider.SetPropertyValue(_registeringDateGuid, value); }
+        }
+      
+        static Guid _descriptionGuid = new Guid("1215da0f-60c3-487b-8d1a-a54f8cd1fb73");
+        [PropertyGuid("1215da0f-60c3-487b-8d1a-a54f8cd1fb73")]
+        public string Description {
+        get { return (string)DataProvider.GetPropertyValue(_descriptionGuid); }
+        set { DataProvider.SetPropertyValue(_descriptionGuid, value); }
+        }
+      
+        static Guid _priceGuid = new Guid("2236e349-c510-4e77-9ac5-df1267d03327");
+        [PropertyGuid("2236e349-c510-4e77-9ac5-df1267d03327")]
+        public int Price {
+        get { return (int)DataProvider.GetPropertyValue(_priceGuid); }
+        set { DataProvider.SetPropertyValue(_priceGuid, value); }
+        }
+      
+        static Guid _numberOfSeatsGuid = new Guid("ebce4c2c-87cc-4d2e-830c-da015db14df5");
+        [PropertyGuid("ebce4c2c-87cc-4d2e-830c-da015db14df5")]
+        public int NumberOfSeats {
+        get { return (int)DataProvider.GetPropertyValue(_numberOfSeatsGuid); }
+        set { DataProvider.SetPropertyValue(_numberOfSeatsGuid, value); }
+        }
+      
+        static Guid _isFreeGuid = new Guid("37b35cbd-b7d3-4322-ac2a-05e84ec8cdfd");
+        [PropertyGuid("37b35cbd-b7d3-4322-ac2a-05e84ec8cdfd")]
+        public bool IsFree {
+        get { return (bool)DataProvider.GetPropertyValue(_isFreeGuid); }
+        set { DataProvider.SetPropertyValue(_isFreeGuid, value); }
+        }
+      
+        static Guid _isFullGuid = new Guid("bb5aebc1-d4e2-4b02-82d2-caf0fb7803c5");
+        [PropertyGuid("bb5aebc1-d4e2-4b02-82d2-caf0fb7803c5")]
+        public bool IsFull {
+        get { return (bool)DataProvider.GetPropertyValue(_isFullGuid); }
+        set { DataProvider.SetPropertyValue(_isFullGuid, value); }
+        }
+      
+        static Guid _instructorsGuid = new Guid("063fed50-4853-4d86-8d8d-dcb54d144e8b");
+        [PropertyGuid("063fed50-4853-4d86-8d8d-dcb54d144e8b")]
+        public IQueryable<WAF.API.MySite.Instructor> Instructors {
+        get {return ((IQueryable<WAF.API.IContent>)DataProvider.GetPropertyValue(_instructorsGuid)).Cast<WAF.API.MySite.Instructor>();}
+        set {DataProvider.SetPropertyValue(_instructorsGuid, value);}
+        }
+      
+        static Guid _participantsGuid = new Guid("56204a02-f18d-46b0-a393-2207c1b504c7");
+        [PropertyGuid("56204a02-f18d-46b0-a393-2207c1b504c7")]
+        public IQueryable<WAF.API.MySite.Participant> Participants {
+        get {return ((IQueryable<WAF.API.IContent>)DataProvider.GetPropertyValue(_participantsGuid)).Cast<WAF.API.MySite.Participant>();}
+        set {DataProvider.SetPropertyValue(_participantsGuid, value);}
+        }
+      
+        static Guid _locationGuid = new Guid("c1f7b8c3-f10a-4ee2-a76f-a3f05b408f5d");
+        [PropertyGuid("c1f7b8c3-f10a-4ee2-a76f-a3f05b408f5d")]
+        public string Location {
+        get { return (string)DataProvider.GetPropertyValue(_locationGuid); }
+        set { DataProvider.SetPropertyValue(_locationGuid, value); }
+        }
+      
+        static Guid _registrationDeadLineGuid = new Guid("952a4b9e-481b-4e24-ac7f-3d8e7e406ff2");
+        [PropertyGuid("952a4b9e-481b-4e24-ac7f-3d8e7e406ff2")]
+        public DateTime RegistrationDeadLine {
+        get { return (DateTime)DataProvider.GetPropertyValue(_registrationDeadLineGuid); }
+        set { DataProvider.SetPropertyValue(_registrationDeadLineGuid, value); }
+        }
+      
+        static Guid _categoriesGuid = new Guid("aa60882a-94a9-4bba-b05a-0909b043b6b2");
+        [PropertyGuid("aa60882a-94a9-4bba-b05a-0909b043b6b2")]
+        public IQueryable<WAF.API.MySite.Category> Categories {
+        get {return ((IQueryable<WAF.API.IContent>)DataProvider.GetPropertyValue(_categoriesGuid)).Cast<WAF.API.MySite.Category>();}
+        set {DataProvider.SetPropertyValue(_categoriesGuid, value);}
+        }
+      
+        static Guid _coursePictureGuid = new Guid("07d321db-3b0d-41c6-bcb3-5cdc0088b9c9");
+        [PropertyGuid("07d321db-3b0d-41c6-bcb3-5cdc0088b9c9")]
+        public PropertyTypes.FileInfo CoursePicture {
+        get { return DataProvider.GetFileInfo(_coursePictureGuid); }
+        set { throw new NotSupportedException(); }
+        }
+      
+}
+}
+
+namespace WAF.API.MySite {
+[ClassGuid("1429c01d-9448-47a1-a435-3752f4231526")]
+public class Instructor : WAF.API.Native.HierarchicalContent {
+public Instructor(IContentDataProvider dataProvider)
+: base(dataProvider) {}
+        static Guid _coursesGuid = new Guid("dfc967a2-0ae1-476e-b185-ba621174ad5a");
+        [PropertyGuid("dfc967a2-0ae1-476e-b185-ba621174ad5a")]
+        public IQueryable<WAF.API.MySite.Course>
+        Courses {
+        get {return ((IQueryable<WAF.API.IContent>)DataProvider.GetPropertyValue(_coursesGuid)).Cast<WAF.API.MySite.Course>();}
+        set {DataProvider.SetPropertyValue(_coursesGuid, value);}
+        }
+      
+        static Guid _descriptionGuid = new Guid("7d6cd5cf-16ed-46cf-a267-dd4cf43edc0f");
+        [PropertyGuid("7d6cd5cf-16ed-46cf-a267-dd4cf43edc0f")]
+        public string Description {
+        get { return (string)DataProvider.GetPropertyValue(_descriptionGuid); }
+        set { DataProvider.SetPropertyValue(_descriptionGuid, value); }
+        }
+      
+        static Guid _linkedinGuid = new Guid("3e4a3fd6-c861-443f-947d-0b6d73d14931");
+        [PropertyGuid("3e4a3fd6-c861-443f-947d-0b6d73d14931")]
+        public string Linkedin {
+        get { return (string)DataProvider.GetPropertyValue(_linkedinGuid); }
+        set { DataProvider.SetPropertyValue(_linkedinGuid, value); }
+        }
+      
+        static Guid _instagramGuid = new Guid("d068c542-8298-4842-b16b-d98d79927b96");
+        [PropertyGuid("d068c542-8298-4842-b16b-d98d79927b96")]
+        public string Instagram {
+        get { return (string)DataProvider.GetPropertyValue(_instagramGuid); }
+        set { DataProvider.SetPropertyValue(_instagramGuid, value); }
+        }
+      
+        static Guid _epostGuid = new Guid("e7220d11-e68d-49f6-b181-b40e0c7aa473");
+        [PropertyGuid("e7220d11-e68d-49f6-b181-b40e0c7aa473")]
+        public string Epost {
+        get { return (string)DataProvider.GetPropertyValue(_epostGuid); }
+        set { DataProvider.SetPropertyValue(_epostGuid, value); }
+        }
+      
+        static Guid _phoneNumberGuid = new Guid("4d97f034-970e-4462-8231-801557edb941");
+        [PropertyGuid("4d97f034-970e-4462-8231-801557edb941")]
+        public int PhoneNumber {
+        get { return (int)DataProvider.GetPropertyValue(_phoneNumberGuid); }
+        set { DataProvider.SetPropertyValue(_phoneNumberGuid, value); }
+        }
+      
+        static Guid _isPublicGuid = new Guid("929a46d6-b5a0-479c-9056-55d14a02a8ac");
+        [PropertyGuid("929a46d6-b5a0-479c-9056-55d14a02a8ac")]
+        public bool IsPublic {
+        get { return (bool)DataProvider.GetPropertyValue(_isPublicGuid); }
+        set { DataProvider.SetPropertyValue(_isPublicGuid, value); }
+        }
+      
+        static Guid _instructorPictureGuid = new Guid("f27eb826-632c-4b83-98d9-3fbb052c3d3c");
+        [PropertyGuid("f27eb826-632c-4b83-98d9-3fbb052c3d3c")]
+        public PropertyTypes.FileInfo InstructorPicture {
+        get { return DataProvider.GetFileInfo(_instructorPictureGuid); }
+        set { throw new NotSupportedException(); }
+        }
+      
+}
+}
+
+namespace WAF.API.MySite {
+[ClassGuid("9dacbbbc-60d9-4edc-baf0-a6cc823a528a")]
+public class Participant : WAF.API.Native.SystemUser {
+public Participant(IContentDataProvider dataProvider)
+: base(dataProvider) {}
+        static Guid _coursesGuid = new Guid("4bc57171-1e3c-4534-ba59-e28164ee259f");
+        [PropertyGuid("4bc57171-1e3c-4534-ba59-e28164ee259f")]
+        public IQueryable<WAF.API.MySite.Course>
+        Courses {
+        get {return ((IQueryable<WAF.API.IContent>)DataProvider.GetPropertyValue(_coursesGuid)).Cast<WAF.API.MySite.Course>();}
+        set {DataProvider.SetPropertyValue(_coursesGuid, value);}
+        }
+      
+        static Guid _allergiesGuid = new Guid("1975cc28-d702-4bec-b504-d4aaa5377fff");
+        [PropertyGuid("1975cc28-d702-4bec-b504-d4aaa5377fff")]
+        public string Allergies {
+        get { return (string)DataProvider.GetPropertyValue(_allergiesGuid); }
+        set { DataProvider.SetPropertyValue(_allergiesGuid, value); }
+        }
+      
+}
+}
+
+namespace WAF.API.MySite {
+[ClassGuid("5f3c2f6c-1e99-4aeb-b3f5-283252fe4387")]
+public class Venue : WAF.API.Native.HierarchicalContent {
+public Venue(IContentDataProvider dataProvider)
+: base(dataProvider) {}
+        static Guid _mapsLinkGuid = new Guid("1aa28122-2253-42f1-bad1-ddf57b44cb36");
+        [PropertyGuid("1aa28122-2253-42f1-bad1-ddf57b44cb36")]
+        public string MapsLink {
+        get { return (string)DataProvider.GetPropertyValue(_mapsLinkGuid); }
+        set { DataProvider.SetPropertyValue(_mapsLinkGuid, value); }
+        }
+      
+        static Guid _adressGuid = new Guid("fd1ce14d-a6e2-4902-bb91-514d1a39e6ac");
+        [PropertyGuid("fd1ce14d-a6e2-4902-bb91-514d1a39e6ac")]
+        public string Adress {
+        get { return (string)DataProvider.GetPropertyValue(_adressGuid); }
+        set { DataProvider.SetPropertyValue(_adressGuid, value); }
+        }
+      
+        static Guid _homeSiteGuid = new Guid("4d04ed51-8d6e-4c63-98e4-c90b1ffddf73");
+        [PropertyGuid("4d04ed51-8d6e-4c63-98e4-c90b1ffddf73")]
+        public string HomeSite {
+        get { return (string)DataProvider.GetPropertyValue(_homeSiteGuid); }
+        set { DataProvider.SetPropertyValue(_homeSiteGuid, value); }
+        }
+      
+        static Guid _venuePictureGuid = new Guid("c5dd1a62-b082-4bf5-aca6-ec2634ea067d");
+        [PropertyGuid("c5dd1a62-b082-4bf5-aca6-ec2634ea067d")]
+        public PropertyTypes.FileInfo VenuePicture {
+        get { return DataProvider.GetFileInfo(_venuePictureGuid); }
+        set { throw new NotSupportedException(); }
+        }
+      
+}
+}
+
+namespace WAF.API.MySite {
+[ClassGuid("94ce9a05-e7cd-426f-821f-967264ab6ed1")]
+public class Category : WAF.API.Native.HierarchicalContent {
+public Category(IContentDataProvider dataProvider)
+: base(dataProvider) {}
+        static Guid _coursesGuid = new Guid("7ce24a13-17d8-47a6-865c-cfa0832d0b6b");
+        [PropertyGuid("7ce24a13-17d8-47a6-865c-cfa0832d0b6b")]
+        public IQueryable<WAF.API.MySite.Course>
+        Courses {
+        get {return ((IQueryable<WAF.API.IContent>)DataProvider.GetPropertyValue(_coursesGuid)).Cast<WAF.API.MySite.Course>();}
+        set {DataProvider.SetPropertyValue(_coursesGuid, value);}
+        }
+      
+}
+}
---
+++ BachelorProsject2023/BachelorProsject2023/Models/WAF/AqlRelations.cs
@@ -0,0 +1,249 @@
+// *******************************************
+//          W A F - AUTO GENERATED CODE       
+// Do not edit! File is regularly overwritten.
+// *******************************************
+
+using WAF.Data.Query;
+using WAF.Engine.Query;
+using WAF.Engine.Query.Advanced;
+
+namespace WAF.Data.Query.MySite {
+
+    public abstract class SqlRelCoursesIntructors {
+
+        public static SqlTable Table {
+            get {
+                return new SqlTable("rel_rel_courses_intructors", TableId.RelationAbstract);
+            }
+        }
+
+        public abstract class Field {
+
+            public static SqlFieldInteger ParentId {
+                get {
+                    return new SqlFieldPrimaryInteger("parent_id", Table);
+                }
+            }
+
+            public static SqlFieldInteger ChildId {
+                get {
+                    return new SqlFieldPrimaryInteger("child_id", Table);
+                }
+            }
+
+            public static SqlFieldInteger Listorder {
+                get {
+                    return new SqlFieldInteger("listorder", Table);
+                }
+            }
+
+        }
+    }
+}
+namespace WAF.Data.Query.MySite {
+
+    public abstract class SqlRelCoursesParticipants {
+
+        public static SqlTable Table {
+            get {
+                return new SqlTable("rel_rel_courses_participants", TableId.RelationAbstract);
+            }
+        }
+
+        public abstract class Field {
+
+            public static SqlFieldInteger ParentId {
+                get {
+                    return new SqlFieldPrimaryInteger("parent_id", Table);
+                }
+            }
+
+            public static SqlFieldInteger ChildId {
+                get {
+                    return new SqlFieldPrimaryInteger("child_id", Table);
+                }
+            }
+
+            public static SqlFieldInteger Listorder {
+                get {
+                    return new SqlFieldInteger("listorder", Table);
+                }
+            }
+
+        }
+    }
+}
+namespace WAF.Data.Query.MySite {
+
+    public abstract class SqlRelCoursesCategories {
+
+        public static SqlTable Table {
+            get {
+                return new SqlTable("rel_rel_courses_categories", TableId.RelationAbstract);
+            }
+        }
+
+        public abstract class Field {
+
+            public static SqlFieldInteger ParentId {
+                get {
+                    return new SqlFieldPrimaryInteger("parent_id", Table);
+                }
+            }
+
+            public static SqlFieldInteger ChildId {
+                get {
+                    return new SqlFieldPrimaryInteger("child_id", Table);
+                }
+            }
+
+            public static SqlFieldInteger Listorder {
+                get {
+                    return new SqlFieldInteger("listorder", Table);
+                }
+            }
+
+        }
+    }
+}
+namespace WAF.Engine.Query.MySite{
+
+    [System.Serializable]
+    public abstract class AqlRelCoursesIntructors{
+        public static AqlClassRelation Relation {
+            get {
+                return new AqlClassRelation( WAFID.GetRelationId("183be1f6-01c2-4789-8220-b0a47f0c4a32") );
+            }
+        }
+
+    }
+}
+namespace WAF.Engine.Query.MySite{
+
+    [System.Serializable]
+    public abstract class AqlRelCoursesParticipants{
+        public static AqlClassRelation Relation {
+            get {
+                return new AqlClassRelation( WAFID.GetRelationId("c01ab5bb-6ba8-4da2-a106-e52e4ce721c5") );
+            }
+        }
+
+    }
+}
+namespace WAF.Engine.Query.MySite{
+
+    [System.Serializable]
+    public abstract class AqlRelCoursesCategories{
+        public static AqlClassRelation Relation {
+            get {
+                return new AqlClassRelation( WAFID.GetRelationId("d0666ab0-c52e-4171-96b6-fcd818f3b4ad") );
+            }
+        }
+
+    }
+}
+namespace WAF.Engine.Query.MySite{
+
+    [System.Serializable]
+    public class AqlAliasRelCoursesIntructors: AqlAliasRelation {
+
+        public AqlAliasRelCoursesIntructors(AqlAlias parent, AqlAlias child)
+             : base(parent, child, AqlRelCoursesIntructors.Relation) {
+
+        }
+        public AqlAliasRelCoursesIntructors(AqlAliasRelation parent, AqlAlias child, AqlAlias onParent)
+             : base(parent, child, AqlRelCoursesIntructors.Relation, onParent ) {
+
+        }
+        public AqlAliasRelCoursesIntructors(AqlAlias parent, AqlAliasRelation child, AqlAlias onChild)
+             : base(parent, child, AqlRelCoursesIntructors.Relation, onChild ) {
+
+        }
+        public AqlAliasRelCoursesIntructors(AqlAliasRelation parent, AqlAliasRelation child,AqlAlias onParent, AqlAlias onChild)
+             : base(parent, child, AqlRelCoursesIntructors.Relation, onParent, onChild ) {
+
+        }
+    }
+}
+namespace WAF.Engine.Query.MySite{
+
+    [System.Serializable]
+    public class AqlAliasRelCoursesParticipants: AqlAliasRelation {
+
+        public AqlAliasRelCoursesParticipants(AqlAlias parent, AqlAlias child)
+             : base(parent, child, AqlRelCoursesParticipants.Relation) {
+
+        }
+        public AqlAliasRelCoursesParticipants(AqlAliasRelation parent, AqlAlias child, AqlAlias onParent)
+             : base(parent, child, AqlRelCoursesParticipants.Relation, onParent ) {
+
+        }
+        public AqlAliasRelCoursesParticipants(AqlAlias parent, AqlAliasRelation child, AqlAlias onChild)
+             : base(parent, child, AqlRelCoursesParticipants.Relation, onChild ) {
+
+        }
+        public AqlAliasRelCoursesParticipants(AqlAliasRelation parent, AqlAliasRelation child,AqlAlias onParent, AqlAlias onChild)
+             : base(parent, child, AqlRelCoursesParticipants.Relation, onParent, onChild ) {
+
+        }
+    }
+}
+namespace WAF.Engine.Query.MySite{
+
+    [System.Serializable]
+    public class AqlAliasRelCoursesCategories: AqlAliasRelation {
+
+        public AqlAliasRelCoursesCategories(AqlAlias parent, AqlAlias child)
+             : base(parent, child, AqlRelCoursesCategories.Relation) {
+
+        }
+        public AqlAliasRelCoursesCategories(AqlAliasRelation parent, AqlAlias child, AqlAlias onParent)
+             : base(parent, child, AqlRelCoursesCategories.Relation, onParent ) {
+
+        }
+        public AqlAliasRelCoursesCategories(AqlAlias parent, AqlAliasRelation child, AqlAlias onChild)
+             : base(parent, child, AqlRelCoursesCategories.Relation, onChild ) {
+
+        }
+        public AqlAliasRelCoursesCategories(AqlAliasRelation parent, AqlAliasRelation child,AqlAlias onParent, AqlAlias onChild)
+             : base(parent, child, AqlRelCoursesCategories.Relation, onParent, onChild ) {
+
+        }
+    }
+}
+namespace WAF.Engine.Content.MySite{
+    [System.Serializable]
+    public partial class RelCoursesIntructors: WAF.Engine.Content.RelationBase {
+
+        public static int RelationId {
+            get {
+                return WAFID.GetRelationId("183be1f6-01c2-4789-8220-b0a47f0c4a32");
+            }
+        }
+
+    }
+}
+namespace WAF.Engine.Content.MySite{
+    [System.Serializable]
+    public partial class RelCoursesParticipants: WAF.Engine.Content.RelationBase {
+
+        public static int RelationId {
+            get {
+                return WAFID.GetRelationId("c01ab5bb-6ba8-4da2-a106-e52e4ce721c5");
+            }
+        }
+
+    }
+}
+namespace WAF.Engine.Content.MySite{
+    [System.Serializable]
+    public partial class RelCoursesCategories: WAF.Engine.Content.RelationBase {
+
+        public static int RelationId {
+            get {
+                return WAFID.GetRelationId("d0666ab0-c52e-4171-96b6-fcd818f3b4ad");
+            }
+        }
+
+    }
+}
