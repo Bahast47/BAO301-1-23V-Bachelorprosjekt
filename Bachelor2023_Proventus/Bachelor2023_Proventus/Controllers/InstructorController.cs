3e78d1457ab8a338ee0e0e3945b10bb4a9de739f
viskog <89581174+viskog@users.noreply.github.com>
2023-02-13 14:57:15 +0100

    Fixed views and controllers for instructor, venue, courselist and course pages

---
+++ Bachelor2023_Proventus/Bachelor2023_Proventus/Controllers/InstructorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Relatude.MVC.Controllers
+{
+    public class InstructorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bachelor2023_Proventus/Bachelor2023_Proventus/Controllers/VenueController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Relatude.MVC.Controllers
+{
+    public class VenueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
